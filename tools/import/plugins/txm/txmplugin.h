9036517035f3c646c6ab1ca2ce6e1718ab2ad567
Ajay Limaye <limaye.ajay@gmail.com>
2016-06-30 15:08:34 +1000

    minor

---
+++ tools/import/plugins/txm/txmplugin.h
@@ -0,0 +1,78 @@
+#ifndef TXMPLUGIN_H
+#define TXMPLUGIN_H
+
+#include <QObject>
+#include "volinterface.h"
+
+#include "pole.h"
+
+class TxmPlugin : public QObject, VolInterface
+{
+  Q_OBJECT
+  Q_PLUGIN_METADATA(IID "drishti.import.Plugin.VolInterface/1.0")
+  Q_INTERFACES(VolInterface)
+
+ public :
+  QStringList registerPlugin();
+
+  void init();
+  void clear();
+
+  bool setFile(QStringList);
+  void replaceFile(QString);
+
+  void gridSize(int&, int&, int&);
+  void voxelSize(float&, float&, float&);
+  QString description();
+  int voxelUnit();
+  int voxelType();
+  int headerBytes();
+
+  QList<uint> histogram();
+  
+  void setMinMax(float, float);
+  float rawMin();
+  float rawMax();
+   
+  void getDepthSlice(int, uchar*);
+  void getWidthSlice(int, uchar*);
+  void getHeightSlice(int, uchar*);
+
+  QVariant rawValue(int, int, int);
+
+  void saveTrimmed(QString, int, int, int, int, int, int);
+
+  void generateHistogram();
+  void set4DVolume(bool);
+ private :
+  POLE::Storage *m_storage;
+
+  QStringList m_fileName;
+  bool m_4dvol;
+  int m_depth, m_width, m_height;
+  int m_voxelUnit;
+  int m_voxelType;
+  int m_headerBytes;
+  float m_voxelSizeX;
+  float m_voxelSizeY;
+  float m_voxelSizeZ;
+  QString m_description;
+  
+  float m_rawMin, m_rawMax;
+  QList<uint> m_histogram;
+
+  QStringList m_imageData;
+
+  int m_bytesPerVoxel;
+
+  int m_dirCount;
+
+  void findMinMaxandGenerateHistogram();
+  void findMinMax();
+
+  void enumerateImages(POLE::Storage*, std::string);
+
+  void loadTxmImage(int, uchar*);
+};
+
+#endif

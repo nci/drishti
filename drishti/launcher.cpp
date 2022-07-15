12422e9b2e770136d8e0d4440d6b86b412994192
Ajay Limaye <limaye.ajay@gmail.com>
2022-07-15 13:42:39 +1000

    launcher added

---
+++ drishti/launcher.cpp
@@ -0,0 +1,55 @@
+#include "launcher.h"
+#include <QDir>
+
+
+Launcher::Launcher(QWidget *parent) :
+  QDialog(parent, Qt::WindowTitleHint|Qt::WindowCloseButtonHint)
+{
+  ui.setupUi(this);
+
+  setStyleSheet("QWidget{background:black;}");
+  ui.drishti->setStyleSheet("QWidget{background:aliceblue;}");
+  ui.drishtiImport->setStyleSheet("QWidget{background:azure;}");
+  ui.drishtiPaint->setStyleSheet("QWidget{background:floralwhite;}");
+
+  QIcon icon;
+  icon.addFile(QString::fromUtf8(":/images/drishti_32.png"), QSize(), QIcon::Normal, QIcon::Off);
+  ui.drishti->setIcon(icon);
+  
+  connect(ui.drishti, SIGNAL(clicked(bool)),
+	  this, SLOT(drishti(bool)));
+  connect(ui.drishtiImport, SIGNAL(clicked(bool)),
+	  this, SLOT(drishtiImport(bool)));
+  connect(ui.drishtiPaint, SIGNAL(clicked(bool)),
+	  this, SLOT(drishtiPaint(bool)));
+}
+
+
+void
+Launcher::drishti(bool b)
+{
+  accept();
+  close();
+}
+
+void
+Launcher::drishtiImport(bool b)
+{
+#if defined(Q_OS_WIN32)
+  QProcess::startDetached(qApp->applicationDirPath() + QDir::separator() + "drishtiimport.exe");
+#else
+  QProcess::startDetached(qApp->applicationDirPath() + QDir::separator() + "drishtiimport");
+#endif
+  exit(0);
+}
+
+void
+Launcher::drishtiPaint(bool b)
+{
+#if defined(Q_OS_WIN32)
+  QProcess::startDetached(qApp->applicationDirPath() + QDir::separator() + "drishtipaint.exe");
+#else
+  QProcess::startDetached(qApp->applicationDirPath() + QDir::separator() + "drishtipaint");
+#endif
+  exit(0);
+}
---
+++ drishti/launcher.h
@@ -0,0 +1,25 @@
+#ifndef LAUNCHER_H
+#define LAUNCHER_H
+
+#include "ui_launcher.h"
+
+#include <QProcess>
+#include <QApplication>
+
+class Launcher : public QDialog
+{
+  Q_OBJECT
+
+ public :
+  Launcher(QWidget *parent=0);
+
+  private slots :
+    void drishti(bool);
+    void drishtiImport(bool);
+    void drishtiPaint(bool);
+
+ private :
+    Ui::launcher ui;
+};
+
+#endif
